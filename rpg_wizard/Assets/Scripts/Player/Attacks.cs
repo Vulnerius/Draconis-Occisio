8c39b8ef0f41eaf5f5ad4eb87d4a00bb9428ccd3
vulnerius <pfuchs480@gmail.com>
2022-05-07 17:39:35 +0200

    :sparkles: FireBall Particle

--- rpg_wizard/Assets/Scripts/Player/Attacks.cs
+++ rpg_wizard/Assets/Scripts/Player/Attacks.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Attacks : MonoBehaviour {
+    [SerializeField] private GameObject fireBall;
     private PlayerControls Controls;
 
     private void Awake() {
@@ -14,16 +15,17 @@ public class Attacks : MonoBehaviour {
 
     private void InitAttackActions(PlayerControls controls) {
         controls.Attacks.FireBall.performed += _ => InstantiateFireBall();
-        
+
         controls.Attacks.WaterShield.performed += _ => InstantiateWaterShield();
     }
 
-    private void InstantiateWaterShield() {
-        
-    }
+    private void InstantiateWaterShield() { }
 
     private void InstantiateFireBall() {
-        
+        //TODO:
+        var fireB = Instantiate(fireBall, transform.position + new Vector3(0,0,1.2f), Quaternion.LookRotation(transform.forward));
+        fireB.GetComponent<Rigidbody>().AddForce(5 * transform.forward, ForceMode.Impulse);
+        Destroy(fireB, 3f);
     }
 
     private void OnDisable() {
