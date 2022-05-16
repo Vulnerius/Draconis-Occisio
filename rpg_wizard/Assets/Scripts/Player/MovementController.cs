53feed1c2614aaa9cdf12c34034cc3dcf41b2c77
vulnerius <pfuchs480@gmail.com>
2022-05-16 12:59:40 +0200

    :sparkles: done with this Movement:poop:

--- rpg_wizard/Assets/Scripts/Player/MovementController.cs
+++ rpg_wizard/Assets/Scripts/Player/MovementController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player {
     public class MovementController : MonoBehaviour {
@@ -16,7 +17,7 @@ namespace Player {
         [Tooltip("The time it takes to turn into a new walking direction. Should be set lower than 0.2")]
         [SerializeField]
         public float turnTime;
-        
+
         [Tooltip("The force applied vertically to the rigidbody when jumping")] [SerializeField]
         public float jumpingForce;
 
@@ -35,49 +36,56 @@ namespace Player {
         }
 
         private void Update() {
+            if (_movement.jumped)
+                StartCoroutine(DoJump());
             if (_movement.moveEnabled)
                 Walk(walkSpeed);
 
             WalkOrIdle();
         }
+        
+        private IEnumerator DoJump() {
+            _movement.moveEnabled = false;
+            OnJump(jumpingForce);
+            yield return new WaitForSeconds(.5f);
+            _movement.moveEnabled = true;
+        }
 
         private void WalkOrIdle() {
-            if (!_movement.moveEnabled)
+            if (!_movement.moveEnabled && !_movement.jumped)
                 StartCoroutine(playIdle());
         }
 
         private IEnumerator playIdle() {
-            yield return new WaitForSeconds(0.75f);
+            yield return new WaitForSeconds(0.1f);
             playerAnimator.Play("Idle03");
         }
 
         private void InitMovementActions(PlayerControls controls) {
             controls.Movement.Move.started += ctx => _movement.moveEnabled = true;
-            controls.Movement.Move.performed += ctx => _movement.move = ctx.ReadValue<Vector2>();
+
+            controls.Movement.Move.performed += ctx => { _movement.move = ctx.ReadValue<Vector2>(); };
             controls.Movement.Move.canceled += _ => {
                 _movement.move = Vector2.zero;
                 _movement.moveEnabled = false;
             };
 
-            controls.Movement.Jump.performed += _ => OnJump(jumpingForce);
+            controls.Movement.Jump.performed += _ => {
+                _movement.jumped = true;
+            };
+            controls.Movement.Jump.canceled += _ => _movement.jumped = false;
 
-            controls.Movement.Sprint.performed += _ => { walkSpeed *= 2; playerAnimator.Play("BattleRunForward");};
+            controls.Movement.Sprint.performed += _ => {
+                walkSpeed *= 2;
+                playerAnimator.Play("BattleRunForward");
+            };
             controls.Movement.Sprint.canceled += _ => walkSpeed /= 2;
         }
 
         private void OnJump(float jumpF) {
-            if (canJump)
-                Jump(jumpF);
-            else
-                canJump = true;
-        }
-
-        private void Jump(float jumpForce) {
             playerAnimator.Play("JumpUp");
-            player.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
-            canJump = false;
+            characterController.Move(Vector3.up * jumpF * Time.fixedDeltaTime);
         }
-
 
         private void Walk(float speed) {
             if (_movement.move.magnitude <= .1f) return;
@@ -102,5 +110,6 @@ namespace Player {
         public Vector2 move;
         public float TurnVelocity;
         public bool moveEnabled;
+        public bool jumped;
     }
 }
